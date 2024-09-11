6e743e7789ce83f1af80f2ea0c568a7cd9806ac6
chenyl8848 <1063415880@qq.com>
2024-09-11 18:51:55 +0800

    feat:思维导图初始化

--- src/pages/office/mindmap.vue
+++ src/pages/office/mindmap.vue
@@ -1,5 +1,59 @@
 <template>
-    <h1>思维导图</h1>
+    <a-card>
+        <p>思维导图</p>
+        <div class="mindMapContainer" ref="mindMapContainerRef"></div>
+    </a-card>
 </template>
 
-<script setup></script>
+<script setup>
+import MindMap from "simple-mind-map"
+import { onMounted, ref } from "vue"
+
+const mindMapContainerRef = ref()
+const mindMapData = {
+    "data": {
+        "text": "根节点"
+    },
+    "children": [{
+        "data": {
+            "text": "Child Node 1",
+            "image": "",
+            "imageTitle": "",
+            "hyperlink": "",
+            "note": ""
+        },
+        "children": []
+    },
+    {
+        "data": {
+            "text": "Child Node 2",
+            "image": "",
+            "imageTitle": "",
+            "hyperlink": "",
+            "note": ""
+        },
+        "children": []
+    }]
+}
+
+
+const initMindMap = () => {
+    const mindMap = new MindMap({
+        el: mindMapContainerRef.value,
+        data: mindMapData
+    })
+}
+
+onMounted(() => {
+    initMindMap()
+})
+</script>
+
+<style lang="scss">
+.mindMapContainer {
+    margin: 0;
+    padding: 0;
+    width: 100%;
+    height: calc(100vh - 320px)
+}
+</style>
