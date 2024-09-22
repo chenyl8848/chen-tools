32ae42380be12658cada6b3c5f0919b794137609
chenyl8848 <1063415880@qq.com>
2024-09-22 21:06:47 +0800

    feat:完成手绘白板

--- src/pages/image/whiteboard.vue
+++ src/pages/image/whiteboard.vue
@@ -1,11 +1,25 @@
 <template>
     <a-card>
         <p class="title">手绘白板</p>
-        <iframe src="https://chencoding.top:8081" class="whiteboardContainer"></iframe>
+        <a-spin :spinning="loading">
+            <iframe src="https://chencoding.top:8081" class="whiteboardContainer" ref="iframeRef" frameborder="0"></iframe>
+        </a-spin>
+        <!-- <a-skeleton loading="loading" >
+            </a-skeleton> -->
     </a-card>
 </template>
 
 <script setup>
+import { onMounted, ref } from 'vue'
+
+const loading = ref(true)
+const iframeRef = ref(null)
+
+onMounted(() => {
+    iframeRef.value.onload = () => {
+        loading.value = false
+    }
+})
 
 </script>
 
@@ -19,6 +33,6 @@
     margin: 0;
     padding: 0;
     width: 100%;
-    height: calc(100vh - 260px)
+    height: calc(100vh - 270px)
 }
 </style>
